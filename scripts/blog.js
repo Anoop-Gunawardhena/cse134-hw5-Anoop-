e1184f59fdd79ef736292bfd81d81b234bc59cc3
Anoop-Gunawardhena <76988884+Anoop-Gunawardhena@users.noreply.github.com>
2023-03-02 06:34:53 -0800

    created crud

---
+++ scripts/blog.js
@@ -0,0 +1,8 @@
+
+    let maincont = document.getElementById('Maincontainer');
+    let addbtn = document.getElementsByTagName('button');
+    if(maincont.childElementCount == 0){
+        let displayp =document.createElement("p");
+        displayp.textContent ="Add some blogs?";
+
+    }
