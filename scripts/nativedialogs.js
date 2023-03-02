f3b8f66dd568d264307aa233fae19f0078a42e97
Anoop-Gunawardhena <76988884+Anoop-Gunawardhena@users.noreply.github.com>
2023-03-01 17:02:09 -0800

    added script

---
+++ scripts/nativedialogs.js
@@ -0,0 +1,5 @@
+const alertbtn = document.getElementById("Alert");
+    alertbtn.addEventListener("click",throwalert);
+function throwalert() {
+        alert("Did you remember to drink water today");
+}
