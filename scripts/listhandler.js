adc1dc439e6019dc9c66df075a02176efbd22e7d
Anoop-Gunawardhena <76988884+Anoop-Gunawardhena@users.noreply.github.com>
2023-03-03 10:57:15 -0800

    changed text formatting and added btnt text

--- scripts/listhandler.js
+++ scripts/listhandler.js
@@ -14,6 +14,11 @@ export function addNewBlog () {
         window.alert("please enter a summary");
         return;
     }
+    if( date.value == 'dd/mm/yyyy'){
+        window.alert('please enter a data');
+    }
+    dateval = data.value
+    lastfour = (dateval).substrng(dateval.length-5,dateval.length-1);
     let listclone = bloglisttemp.content.cloneNode(true)
     let list =  listclone.querySelector('ul');
     maincont.appendChild(list);
@@ -22,14 +27,16 @@ export function addNewBlog () {
     let itemdesc = document.createElement('p');
     let editbtn = document.createElement('button');
     editbtn.class = 'editbtn'
+    editbtn.innerText = 'Edit'
     let removebtn = document.createElement('button')
     removebtn.class = 'removebtn'
+    removebtn.innerText = 'Remove'
     bloglistitem.class = 'listitems';
     bloglistitem.id = count.toString();
     editbtn.id = 'editbtn'+ count.toString();
     removebtn.id = 'removebrn'+ count.toString();
     count = count + 1;
-    itemdesc.value = title.value + ' ( ' + date.value + ' ) - ' + summary.value;  
+    itemdesc.innerText= title.value + ' ( ' + lastfour + ' ) - ' + summary.value;  
     bloglistitem.append(itemdesc);
     bloglistitem.append(editbtn);
     bloglistitem.append(removebtn);
