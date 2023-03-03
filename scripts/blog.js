0a55f1741202f0376b4c72339c5720093ad436fb
Anoop-Gunawardhena <76988884+Anoop-Gunawardhena@users.noreply.github.com>
2023-03-02 21:34:45 -0800

    form added

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,8 +1,26 @@
 
-    let maincont = document.getElementById('Maincontainer');
+    let maincont = document.getElementById('listcontainer');
+    let bodycont = document.getElementById('bodycontainer');
     let addbtn = document.getElementsByTagName('button');
+    let formtemplate = document.querySelector('template');
+    console.log(maincont.childElementCount);
     if(maincont.childElementCount == 0){
+        bodycont.style.display = 'flex';
+        bodycont.style.flexDirection = 'row';
+        bodycont. style.gap = '10px'
+        setTimeout(function(){
         let displayp =document.createElement("p");
         displayp.textContent ="Add some blogs?";
+    maincont.appendChild(displayp);
 
-    }
+    },0);
+}
+function startblogs(){
+    let currentgap = bodycont.style.gap
+    bodycont.style.display.flexDirection = 'column'
+    let formclone = formtemplate.content.cloneNode(true)
+    let form =  formclone.querySelector('form');
+    maincont.append(form);
+
+}
+addbtn.addEventListener("click",startblogs);
