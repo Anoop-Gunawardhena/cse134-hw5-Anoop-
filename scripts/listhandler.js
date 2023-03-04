e8da77bfebf07a17b50c32ead4cab744dd9310d4
Anoop-Gunawardhena <76988884+Anoop-Gunawardhena@users.noreply.github.com>
2023-03-03 22:02:15 -0800

    enhanced debugging

--- scripts/listhandler.js
+++ scripts/listhandler.js
@@ -52,8 +52,8 @@ export function buildList(){
     for ( let i = 0 ; i < localStorage.length; i++){
         let key = localStorage.key[i];
         let data = localStorage.getItem(key);
-        console.log("key is " + key)
-        console.log('data is ' + data);
+        console.log("key is " + key.toString())
+        console.log('data is ' + data.toString());
         createItem(key, data, list);
     }
 }
